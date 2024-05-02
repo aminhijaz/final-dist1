0f1a6342a378eb0d989f25ae1b256edab65a0422
aminhijaz <aminhijaz2001@gmail.com>
2024-05-02 02:32:17 -0400

    HI

--- distribution/all/mr.js
+++ distribution/all/mr.js
@@ -1,5 +1,9 @@
 const distribution = require('../../distribution');
 const fetch = require('node-fetch');
+<<<<<<< HEAD
+=======
+const { publicIp, publicIpv4, publicIpv6 } = require('public-ip');
+>>>>>>> e58770dad20c2008c237a734fcd4eeca0a2c864c
 
 function createListener(nNodes, id, gid, callback) {
   return {
